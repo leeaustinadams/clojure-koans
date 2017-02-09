ff7a63685d4f4d1d2a41a1f1da3e18574fbd631e
Lee Adams <ladams@twitter.com>
2017-02-09 14:55:18 -0800

    Complete the newly added koans

--- src/koans/06_maps.clj
+++ src/koans/06_maps.clj
@@ -42,10 +42,10 @@
   (= {1 "January"} (dissoc {1 "January" 2 "February"} 2))
 
   "Create a new map by merging"
-  (= {:a 1 :b 2 __ __} (merge {:a 1 :b 2} {:c 3}))
+  (= {:a 1 :b 2 :c 3} (merge {:a 1 :b 2} {:c 3}))
 
   "Specify how to handle entries with same keys when merging"
-  (= {:a 1 :b __ :c 3} (merge-with + {:a 1 :b 1} {:b 1 :c 3}))
+  (= {:a 1 :b 2 :c 3} (merge-with + {:a 1 :b 1} {:b 1 :c 3}))
 
   "Often you will need to get the keys, but the order is undependable"
   (= (list 2010 2014 2018)
@@ -53,10 +53,10 @@
 
   "You can get the values in a similar way"
   (= (list "PyeongChang" "Sochi" "Vancouver")
-     (sort (vals {2010 "Vancouver" 2014 "Sochi" 2018 "PyeongChang"}))))
+     (sort (vals {2010 "Vancouver" 2014 "Sochi" 2018 "PyeongChang"})))
 
   "You can even iterate over the map entries as a seq"
-  (= {:a __ :b __}
+  (= {:a 2 :b 3}
      (into {}
            (map
             (fn [[k v]] [k (inc v)])
